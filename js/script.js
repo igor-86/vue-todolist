decb2a13c9096c04c5127c0f22c3d9392b7c724c
igor-86 <i.campoli86@gmail.com>
2022-10-27 22:44:08 +0200

    Bonus 2

--- js/script.js
+++ js/script.js
@@ -2,10 +2,41 @@ const {createApp} = Vue;
 createApp({
     data(){
         return{
-            message : "hello"
+            todoList: [
+                {
+                    text: "Comprare le scarpe",
+                    done: false
+                },
+                {
+                    text: "Fare l bucato",
+                    done: false
+                },
+                {
+                    text: "Cantare",
+                    done: false
+                }
+            ],
+            inserList: "",
         }
     },
     methods: {
+        isDone: function(index){
+            if(this.todoList[index].done === false){
+                this.todoList[index].done = true
+            } else if(this.todoList[index].done === true){
+                this.todoList[index].done = false;
+            }
+        },
+        addText: function(){
+            if(this.inserList){
+                this.todoList.push({text:this.inserList, done:false});
+                this.inserList="";
+            }
+        },
+        removeInList: function(indexOfList){
+            this.todoList.splice(indexOfList, 1);
+        },
+        
         
     }
 }).mount("#app");
